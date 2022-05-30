4f095b50846202d116c593e41133587ff5f11618
vmisovic <vladimir.misovic03@gmail.com>
2022-05-30 20:18:45 +0200

    4spaces -> tab

--- src/igra.h
+++ src/igra.h
@@ -10,18 +10,18 @@
 #define NA_MESTU 5
 class igra
 {
-    sf::RenderWindow *prozor;
-    sf::Texture tZid, tKutija, tKraj, tIgrac, tNaMestu;
-    sf::Sprite zid, kutija, kraj, igrac, na_mestu;
-    sf::Color boja[2];
-    int matrica[16][19], mapa[16][19];
-    sf::Vector2f kutija_vektor[35], igrac_vektor;
-    bool kutija_na_mestu[35];
-    int br_kutija;
+	sf::RenderWindow *prozor;
+	sf::Texture tZid, tKutija, tKraj, tIgrac, tNaMestu;
+	sf::Sprite zid, kutija, kraj, igrac, na_mestu;
+	sf::Color boja[2];
+	int matrica[16][19], mapa[16][19];
+	sf::Vector2f kutija_vektor[35], igrac_vektor;
+	bool kutija_na_mestu[35];
+	int br_kutija;
 public:
-    igra(sf::RenderWindow* prozor1);
-    bool ucitaj_nivo(int br);
-    void ispis_matrice();
-    void ispis_podataka();
+	igra(sf::RenderWindow* prozor1);
+	bool ucitaj_nivo(int br);
+	void ispis_matrice();
+	void ispis_podataka();
 	void nivo();
 };
--- src/source.cpp
+++ src/source.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-    sf::RenderWindow prozor(sf::VideoMode(1400, 800), "sokoban", sf::Style::Close);
-    prozor.setPosition(sf::Vector2i(100, 0));
-    igra sokoban(&prozor);
-    sokoban.nivo();
+	sf::RenderWindow prozor(sf::VideoMode(1400, 800), "sokoban", sf::Style::Close);
+	prozor.setPosition(sf::Vector2i(100, 0));
+	igra sokoban(&prozor);
+	sokoban.nivo();
 
 
-    return 0;
+	return 0;
 }
